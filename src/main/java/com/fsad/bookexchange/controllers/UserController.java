8c81736e874ce8d18c119c2a0ff06dce3a9b1bb6
Ishan Jain <2023MT93333@wilp.bits-pilani.ac.in>
2024-10-30 13:13:12 +0530

    Adding-controller

---
+++ src/main/java/com/fsad/bookexchange/controllers/UserController.java
@@ -0,0 +1,42 @@
+package com.fsad.bookexchange.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fsad.bookexchange.entity.User;
+import com.fsad.bookexchange.service.UserService;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+
+    @PostMapping("/users")
+    public ResponseEntity<?> addUsers(@RequestBody User user) {
+        if (userService.isUserNameAlreadyExists(user.getUsername())) {
+            return new ResponseEntity<>("UserName already exists- try with different username!", HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(userService.addUser(user), HttpStatus.CREATED);
+    }
+
+
+}
